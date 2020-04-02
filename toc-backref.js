3a0991699c9cec1895499163fb79efa2c84a1f2e
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-04-02 23:15:49 +0800

    Format js file

--- toc-backref.js
+++ toc-backref.js
@@ -1,12 +1,12 @@
 (($) => {
-     String.prototype.format = function() {
-         var formatted = this;
-         for (var i = 0; i < arguments.length; i++) {
-             var regexp = new RegExp('\\{'+i+'\\}', 'gi');
-             formatted = formatted.replace(regexp, arguments[i]);
-         }
-         return formatted;
-     };
+  String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+      var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+      formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+  };
 
   let generateBackRef = (tocUlSelector, contentSelector, hTagsArr, modest=true) => {
     var tocUl = $(tocUlSelector), tocId, tocBackRef;
@@ -49,45 +49,45 @@
     });
   };
 
-    let generateToc = ( contentSelector, hTagsArr, modest=true) => {
-        var tocUl = $('<ul class="toc-body"></ul>'), tocId, tocLi, tocRef, tocBackRef;
-        var tocIdPrefix = "toc";
-        var content = $(contentSelector), hId, hText, hTagName;
-        var hList = content.find(hTagsArr.join(','));
+  let generateToc = ( contentSelector, hTagsArr, modest=true) => {
+    var tocUl = $('<ul class="toc-body"></ul>'), tocId, tocLi, tocRef, tocBackRef;
+    var tocIdPrefix = "toc";
+    var content = $(contentSelector), hId, hText, hTagName;
+    var hList = content.find(hTagsArr.join(','));
 
-        hList.each(function (index) {
-            tocId = tocIdPrefix + index;
-            hText = $(this).text();
+    hList.each(function (index) {
+      tocId = tocIdPrefix + index;
+      hText = $(this).text();
 
-            if (modest) {
-                tocBackRef = $("<a class='toc-backref p-1' href='#{0}' rel='nofollow' target='_self'></a>".format(tocId));
-            } else {
-                tocBackRef = $("<a class='toc-backref p-1' href='#{0}' rel='nofollow' target='_self'>{1}</a>".format(tocId, hText));
-                $(this).text("");
-            }
+      if (modest) {
+        tocBackRef = $("<a class='toc-backref p-1' href='#{0}' rel='nofollow' target='_self'></a>".format(tocId));
+      } else {
+        tocBackRef = $("<a class='toc-backref p-1' href='#{0}' rel='nofollow' target='_self'>{1}</a>".format(tocId, hText));
+        $(this).text("");
+      }
 
-            $(this).append(tocBackRef);
+      $(this).append(tocBackRef);
 
-            hId = $(this).prop('id');
-            if (hId == undefined || hId == '' || hId == null) {
-                var headingIdPrefix = "header";
-                $(this).attr("id", headingIdPrefix + index);
-                hId = $(this).prop('id');
-            } else {
-                // pass
-            }
+      hId = $(this).prop('id');
+      if (hId == undefined || hId == '' || hId == null) {
+        var headingIdPrefix = "header";
+        $(this).attr("id", headingIdPrefix + index);
+        hId = $(this).prop('id');
+      } else {
+        // pass
+      }
 
-            tocRef = $('<a class="reference internal" id="{0}" href="#{1}">{2}</a>'.format(tocId, hId, hText));
-            hTagName = $(this).prop('tagName').toLowerCase();
-            tocLi = $('<li class="{0}_nav"></li>'.format(hTagName)).append(tocRef);
-            tocUl.append(tocLi);
-        });
+      tocRef = $('<a class="reference internal" id="{0}" href="#{1}">{2}</a>'.format(tocId, hId, hText));
+      hTagName = $(this).prop('tagName').toLowerCase();
+      tocLi = $('<li class="{0}_nav"></li>'.format(hTagName)).append(tocRef);
+      tocUl.append(tocLi);
+    });
 
-        $(contentSelector).prepend(tocUl);
+    $(contentSelector).prepend(tocUl);
 
-        return true;
-    };
+    return true;
+  };
 
-    $.extend({ generateToc, generateBackRef });
+  $.extend({ generateToc, generateBackRef });
 
 })($);
